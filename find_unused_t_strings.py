0a2b9fe13728f71ced9e87b4dfbb2f3aa0c9f973
Toon Van de Putte <toon@automaton.be>
2017-09-07 00:10:04 +0200

    Find and list unused translation keys

---
+++ find_unused_t_strings.py
@@ -0,0 +1,30 @@
+import json
+
+with open('app/translations.js') as data_file:
+    clean_json_txt = data_file.read().replace(");","")
+    clean_json_txt = clean_json_txt.replace("angular.module('app').constant('translations', ","").replace("'","\"").replace("\\","").replace("</","").replace("<","").replace(">","").replace("\n","").replace("\r","")
+    #print(clean_json_txt)
+    data = json.loads(clean_json_txt)
+
+keyslist = dict()
+
+for key in data.keys():
+    keyslist[key] = 0
+
+filenames = [
+    'app/submissions/submission.html',
+    'app/submissions/collaborator-dialog.html'
+]
+
+for filename in filenames:
+    with open(filename) as template_file:
+        template_file_string = template_file.read()
+        for key in keyslist:
+            if template_file_string.find("'"+key+"'") >= 0:
+                keyslist[key] += 1
+
+print ("unused keys:")
+
+for key,val in keyslist.items():
+    if 0 == val:
+        print (key)
